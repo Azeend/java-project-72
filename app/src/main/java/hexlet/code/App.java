43d91489a79e7c20839091237007b4d5629c89b4
Azeend <marat9466@gmail.com>
2023-05-24 11:35:28 +0300

    step 2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,17 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+import io.javalin.core.JavalinConfig;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Javalin app = getApp();
+        app.start(8080);
+    }
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(JavalinConfig::enableDevLogging)
+                .get("/", ctx -> ctx.result("Hello World"));
+        return app;
     }
     public static String forTest() {
         return "Im worked";
